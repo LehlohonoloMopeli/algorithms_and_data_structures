07d59b45daf7289efee0442e22c4268194b252f6
Lehlohonolo Mopeli <lehlohonolompl@gmail.com>
2022-07-05 14:33:02 +0200

    repackaged algorithms

---
+++ linked_lists/linked_lists.py
@@ -0,0 +1,20 @@
+"""
+    Notes on linked lists:
+        1. A linked list is a data structure that consists of a group of nodes
+        2. Each node in a linked list contains data and a pointer to the next node
+        3. The last node in a linked list does not have a pointer to the next node
+        4. Linked lists need more memory than arrays because they store data and references to other nodes
+        5. The first node in a linked list is called the head node
+        6. The last node in a linked list is called the tail node
+        7. Finding an arbitrary item in a linked list is O(n) linear time
+        
+    Advantages:
+        1. Linked lists are faster than arrays
+        2. Linked lists are more flexible than arrays
+        3. Efficient in inserting and deleting elements
+        4. It cannot have holes in the data so there is no need to shift items around
+        5. Items are not stored next to each other in memory so there is no random indexing
+        6. Linked lists allow us to create more complex data structures such as stacks and queues
+        7. inserting items at the beginning of the data structure is O(1)
+
+"""
